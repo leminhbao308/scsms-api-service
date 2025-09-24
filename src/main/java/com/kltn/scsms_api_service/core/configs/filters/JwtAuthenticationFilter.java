ec84115d305d6702273ac03dd69c096e7f0aec43
Lê Minh Bảo <besilent308@gmail.com>
2025-09-24 16:28:36 +0700

    fix(jwt): Fix jwt filter

--- src/main/java/com/kltn/scsms_api_service/core/configs/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/kltn/scsms_api_service/core/configs/filters/JwtAuthenticationFilter.java
@@ -36,6 +36,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     
     private static String API_PREFIX = "/api";
     
+    private static final List<String> PROTECTED_PATH_PATTERNS =
+        List.of(
+            API_PREFIX + ApiConstant.LOGOUT_API, // Protect logout endpoint
+            API_PREFIX + ApiConstant.VERIFY_TOKEN_API // Protect verify token endpoint
+        );
+    
     private final AntPathMatcher pathMatcher = new AntPathMatcher();
     
     @Override
@@ -46,9 +52,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         throws ServletException, IOException {
         
         String path = request.getRequestURI();
+        boolean isProtected =
+            PROTECTED_PATH_PATTERNS.stream().anyMatch(pattern -> pathMatcher.match(pattern, path));
         
         // Skip authentication for non-protected paths
-        if (isBypassPath(request.getRequestURI())) {
+        if (!isProtected || isBypassPath(path)) {
             filterChain.doFilter(request, response);
             return;
         }
