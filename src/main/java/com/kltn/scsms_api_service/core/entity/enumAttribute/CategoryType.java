78b255b678947ee7ebd6b4b7f29ea564592b0685
NAMLEHOANG\lehoa <lehoangnam.31123@gmail.com>
2025-09-30 23:55:42 +0700

    feat(Category): update type for category and fix constraint in table

--- src/main/java/com/kltn/scsms_api_service/core/entity/enumAttribute/CategoryType.java
+++ src/main/java/com/kltn/scsms_api_service/core/entity/enumAttribute/CategoryType.java
@@ -1,5 +1,9 @@
 package com.kltn.scsms_api_service.core.entity.enumAttribute;
 
 public enum CategoryType {
-    PRODUCT, SERVICE
+    PRODUCT, 
+    SERVICE,
+    SERVICE_SYS,
+    PRODUCT_SYS,
+    PROMOTION_SYS
 }
