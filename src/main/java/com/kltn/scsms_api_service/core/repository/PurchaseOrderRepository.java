f8e8efdc37a873ccd4446444bcd3be60b1345b5b
Lê Minh Bảo <besilent308@gmail.com>
2025-10-14 05:30:12 +0700

    feat(purchase order): add export functionality for purchase order report in Excel format

--- src/main/java/com/kltn/scsms_api_service/core/repository/PurchaseOrderRepository.java
+++ src/main/java/com/kltn/scsms_api_service/core/repository/PurchaseOrderRepository.java
@@ -3,7 +3,18 @@ package com.kltn.scsms_api_service.core.repository;
 import com.kltn.scsms_api_service.core.entity.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, UUID> {
+    List<PurchaseOrder> findByBranch_BranchId(UUID branchBranchId);
+    
+    List<PurchaseOrder> findByCreatedDateLessThanEqualAndBranch_BranchId(LocalDateTime createdDateIsLessThan, UUID branchBranchId);
+    
+    List<PurchaseOrder> findByCreatedDateGreaterThanEqualAndBranch_BranchId(LocalDateTime createdDateIsGreaterThan, UUID branchBranchId);
+    
+    List<PurchaseOrder> findByCreatedDateBetween(LocalDateTime createdDateAfter, LocalDateTime createdDateBefore);
+    
+    List<PurchaseOrder> findByCreatedDateBetweenAndBranch_BranchId(LocalDateTime createdDateAfter, LocalDateTime createdDateBefore, UUID branchBranchId);
 }
